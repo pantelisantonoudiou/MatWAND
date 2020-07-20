cc29eac39a601b55fd8f4a986c2f7ec6ddc86e69
Pantelis Antonoudiou <pantelis.antonoudiou@gmail.com>
2020-07-20 11:14:51 -0400

    add unique ids

---
+++ add_unique_id.m
@@ -0,0 +1,37 @@
+function add_unique_id(file_path,ext)
+% add_unique_id(file_path,ext)
+% add_unique_id(file_path,'.mat')
+
+% % get file path
+% % file_path = 'C:\Users\panton01\Desktop\paper_data\fig2_allo_mice\raw_data_BLA';
+% % file extension
+% % ext = '.mat';
+
+% get file list
+file_list = dir(fullfile(file_path, ['*', ext]));
+
+disp('Renaming...')
+disp('_____________________________________')
+
+for i = 1:length(file_list) % loop through files
+
+    temp_file = erase(file_list(i).name, ext); % remove ending
+    k =  strfind(temp_file,'_'); % find underscore position
+    
+    if length(k) == 0 %#ok no underscores
+        new_name = horzcat(temp_file, ['_', num2str(i), '_'], ext);
+    elseif length(k) == 1 % one underscore for condition
+        new_name = horzcat(temp_file(1:k), [num2str(i), '_'], temp_file(k+1:end) ,ext);
+    elseif length(k) > 1
+        new_name = 'More than one underscore detected: File name will not be changed.';
+    end
+    
+    if contains(new_name, ext) % rename file
+        movefile(fullfile(file_path,file_list(i).name),fullfile(file_path,new_name))
+    end
+    fprintf('from %s -----> %s \n',file_list(i).name,new_name)
+
+end
+disp('File have been renamed with unique IDs.')
+end
+
